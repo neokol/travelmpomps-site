2cadc8b82401e1c1a4e67f65bad0b9820c872f42
neokol <neokliskol@protonmail.com>
2025-07-16 11:29:22 +0300

    Update Vienna navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -59,6 +59,13 @@ function Navbar() {
                                     >
                                         Βαρκελώνη
                                     </Link>
+                                    <Link
+                                        href="/destinations/vienna"
+                                        onClick={handleNavItemClick}
+                                        className="block px-4 py-2 text-[#2E2E2F] hover:bg-gray-100"
+                                    >
+                                        Βιέννη
+                                    </Link>
                                     {/* Add more dropdown links as needed */}
                                 </div>
                             )}
