8245d72c7d980070c513fe6ce4ce5a7e32aa3fde
neokol <neokliskol@protonmail.com>
2025-07-25 12:52:19 +0300

    Update DestinationsSection image

--- app/components/DestinationSection.tsx
+++ app/components/DestinationSection.tsx
@@ -6,7 +6,7 @@ export default function DestinationsSection() {
         <section className="py-16 px-4 sm:px-6 lg:px-8">
             <div className="relative w-full h-[300px] md:h-[400px] lg:h-[600px] mx-auto max-w-6xl">
                 <Image
-                    src="/images/1.jpg"
+                    src="/images/Enjoy_travel.jpg"
                     alt="Travel Quote"
                     fill
                     className="object-cover rounded-2xl md:rounded-3xl shadow-xl"
