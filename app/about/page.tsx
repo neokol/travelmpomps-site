7118974d82ce2db1ec8e5c296ed0f42702b0b382
neokol <neokliskol@protonmail.com>
2025-07-29 13:28:52 +0300

    Update About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -25,24 +25,9 @@ function About() {
 
             {/* About Content Section with background image */}
             <div className="relative w-full h-[80vh] pt-20 md:pt-24"> {/* Adjusted pt-20 for mobile screens */}
-                {/* Using Next.js Image for background */}
-                <div className="absolute inset-0">
-                    <Image
-                        src="/travel_the_world.jpg"  // Make sure to replace with your actual image path
-                        alt="Travel the World"
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="center"
-                        quality={100}
-                    />
-                </div>
-
-                {/* Overlay */}
-                <div className="absolute inset-0 bg-[#F8F6FE]/80"></div>
-
                 {/* Content */}
-                <div className="absolute inset-0 flex justify-center items-center text-[#010C14] px-4 md:mt-30 sm:mt-170">
-                    <div className="text-center max-w-3xl p-8 rounded-lg">
+                <div className="absolute inset-x-0 top-16 md:top-20 flex justify-center items-center text-[#010C14] px-4">
+                    <div className="text-center max-w-3xl p-8 sm:p-8 rounded-lg">
                         <h1 className="text-4xl font-semibold mb-4">Λίγα λόγια για μάς...</h1>
                         <p className="mb-4 text-lg">Το travelmpomps ξεκίνησε από το πάθος μας για τα ταξίδια. Η αρχή έγινε με ένα ταξίδι στην Ρώμη, όπου αποφασίσαμε να μοιραστούμε την αγάπη μας για τις νέες εμπειρίες δημιουργώντας μικρά βίντεο στο TikTok. Στόχος μας ήταν να δείξουμε στον κόσμο τα αξιοθέατα, τα κρυφά σημεία και τις μοναδικές τοποθεσίες που ανακαλύπταμε σε κάθε προορισμό.</p>
                         <p className="mb-4 text-lg">Με τον καιρό, η επιθυμία μας να μοιραστούμε τις εμπειρίες μας επεκτάθηκε, και έτσι ξεκινήσαμε να δημιουργούμε περιεχόμενο όχι μόνο για τα ταξίδια μας στο εξωτερικό, αλλά και για τις εκδρομές μας στην Ελλάδα. Εστιάσαμε σε αγαπημένα μέρη, από παραδοσιακά εστιατόρια και street food μέχρι μοναδικά σημεία και αξιοθέατα που επισκεπτόμαστε κατά τη διάρκεια των περιηγήσεών μας.</p>
@@ -50,7 +35,17 @@ function About() {
                     </div>
                 </div>
             </div>
-
+            <section className="py-16 px-4 sm:px-6 lg:px-8 mt-30">
+                        <div className="relative w-full h-[300px] md:h-[400px] lg:h-[600px] mx-auto max-w-6xl">
+                            <Image
+                                src="/images/toscany_1.jpg"
+                                alt="Travel Quote"
+                                fill
+                                className="object-cover rounded-2xl md:rounded-3xl shadow-xl"
+                                priority
+                            />
+                        </div>
+            </section>
             {/* Footer Section */}
             <div className="pt-70">
                 <PageFooter />
